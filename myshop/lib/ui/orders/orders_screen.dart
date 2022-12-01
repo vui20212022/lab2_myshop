bd88cb5f544e96785a48c517798ee74ee2eacf9b
nguyenvanvui <nvvui90@gmail.com>
2022-12-01 18:32:07 +0700

    Xay dung trang cac dat hang

---
+++ myshop/lib/ui/orders/orders_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../shared/app_drawer.dart';
+import'order_manager.dart';
+import 'order_item_card.dart';
+
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    print('building orders');
+    final ordersManager = OrdersManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      drawer: const AppDrawer(),
+      body: ListView.builder(
+        itemCount: ordersManager.orderCount,
+        itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+      ),
+    );
+  }
+}
