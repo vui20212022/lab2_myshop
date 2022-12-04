0bf46b83f0a612270288b04f6bf68421214c454f
nguyenvanvui <nvvui90@gmail.com>
2022-12-04 08:02:15 +0700

    Xay dung trang hieu chinh san pham

--- myshop/lib/ui/shared/dialog_utils.dart
+++ myshop/lib/ui/shared/dialog_utils.dart
@@ -1,25 +1,43 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Future<bool?> showConfirmDialog(BuildContext context, String massage){
+Future<bool?> showConfirmDialog(BuildContext context, String message) {
   return showDialog(
     context: context,
-     builder: (ctx) => AlertDialog(title: const Text('are you sure?'),
-     content: Text(massage),
-     actions: <Widget>[
-    TextButton(
-        child: const Text('no'),
-        onPressed:() {
-        Navigator.of(ctx).pop(false);
-      },
+    builder: (ctx) => AlertDialog(
+      title: const Text('Are you sure?'),
+      content: Text(message),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('No'),
+          onPressed: () {
+            Navigator.of(ctx).pop(false);
+          },
+        ),
+        TextButton(
+          child: const Text('Yes'),
+          onPressed: () {
+            Navigator.of(ctx).pop(true);
+          },
+        ),
+      ],
     ),
-   TextButton(
-        child: const Text('yes'),
-        onPressed:() {
-        Navigator.of(ctx).pop(true);
-        },
-      ),
-    ],
-  ),
- );
+  );
+}
+
+Future<bool?> showErrorDialog(BuildContext context, String message) {
+  return showDialog(
+    context: context,
+    builder: (ctx) => AlertDialog(
+      title: const Text('An Error Occurred!'),
+      content: Text(message),
+      actions: <Widget>[
+        TextButton(
+          child: const Text('Okay'),
+          onPressed: () {
+            Navigator.of(ctx).pop();
+          },
+        ),
+      ],
+    ),
+  );
 }
