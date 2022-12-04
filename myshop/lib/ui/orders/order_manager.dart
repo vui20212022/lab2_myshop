2a328d8575894b4f78e9438a65d98ef82e198510
nguyenvanvui <nvvui90@gmail.com>
2022-12-04 07:52:54 +0700

    Quan ly trang thai voi provider - OrdersManager

--- myshop/lib/ui/orders/order_manager.dart
+++ myshop/lib/ui/orders/order_manager.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 
 import '../../models/cart_item.dart';
 import '../../models/order_item.dart';
-
 
 class OrdersManager with ChangeNotifier {
   final List<OrderItem> _orders = [
@@ -15,15 +14,30 @@ class OrdersManager with ChangeNotifier {
           title: 'Red Shirt',
           price: 29.99,
           quantity: 2,
-          )
-        ],
-        dateTime:DateTime.now(),
-      )
-    ];
-    int get orderCount {
-      return _orders.length;
-    }
-    List<OrderItem> get orders {
-      return [..._orders];
-    }
+        )
+      ],
+      dateTime: DateTime.now(),
+    )
+  ];
+
+  int get orderCount {
+    return _orders.length;
+  }
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+
+  void addOrder(List<CartItem> cartProducts, double total) async {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: 'o${DateTime.now().toIso8601String()}',
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+}
 }
--- myshop/lib/ui/orders/orders_screen.dart
+++ myshop/lib/ui/orders/orders_screen.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../shared/app_drawer.dart';
-import'order_manager.dart';
+import 'order_manager.dart';
 import 'order_item_card.dart';
-
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
   const OrdersScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     print('building orders');
     final ordersManager = OrdersManager();
     return Scaffold(
@@ -17,9 +17,17 @@ class OrdersScreen extends StatelessWidget {
         title: const Text('Your Orders'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
+      /* body: ListView.builder(
         itemCount: ordersManager.orderCount,
         itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+      ), */
+      body: Consumer<OrdersManager>(
+        builder: (ctx, ordersManager, child) {
+          return ListView.builder(
+            itemCount: ordersManager.orderCount,
+            itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+          );
+        },
       ),
     );
   }
